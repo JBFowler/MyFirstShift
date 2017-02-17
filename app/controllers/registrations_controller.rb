c17cd8bfe8dfd57fe3a089c4c57a672a941ef601
Joe Fowler <jbfowler2@crimson.ua.edu>
2017-02-16 23:41:41 -0600

    Updated registrations route

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,24 +1,24 @@
-class RegistrationsController < ApplicationController
+class RegistrationsController < Devise::RegistrationsController
 
-  def new
-    @organization = Organization.new
-    @organization.users.build
-  end
+  # def new
+  #   @organization = Organization.new
+  #   @organization.users.build
+  # end
 
-  def create
-    @organization = Organization.new(organization_params)
+  # def create
+  #   @organization = Organization.new(organization_params)
 
-    if @organization.save
-      flash["success"] = "Your profile and organization were successfully created"
-      redirect_to home_path(subdomain: @organization.subdomain)
-    else
-      render :new
-    end
-  end
+  #   if @organization.save
+  #     flash["success"] = "Your profile and organization were successfully created"
+  #     redirect_to home_path(subdomain: @organization.subdomain)
+  #   else
+  #     render :new
+  #   end
+  # end
 
   private
 
-  def organization_params
+  def sign_up_params
     params.require(:organization).permit(:name, :size, :sector, :subdomain, users_attributes: [:first_name, :last_name, :email, :username, :password])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,30 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: [:sessions, :registrations], :controllers => {:registrations => "registrations"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'front#index'
 
-  get 'register', to: 'registrations#new'
-  post 'register', to: 'registrations#create'
-
-  namespace :sign_in do
-    get '/', to: 'organization#find_subdomain'
-    get 'find', to: 'organization#find_user', as: :find_user
-  end
-
   constraints(subdomain: "www") do
     get '/', to: 'front#index'
+    devise_scope :user do
+      get '/register', to: 'registrations#new'
+      post'/register', to: 'registrations#create'
+    end
+
+    namespace :sign_in do
+      get '/', to: 'organization#find_subdomain'
+      get '/find', to: 'organization#find_user', as: :find_user
+    end
   end
 
   constraints(Subdomain) do
-    scope module: 'organizations' do
-      root to: 'sessions#new'
-      
-      post '/', to: 'sessions#create'
-      get 'sign_out', to: 'sessions#destroy'
+    devise_scope :user do
+      get '/', to: 'devise/sessions#new', as: :new_user_session
+      post '/', to: 'devise/sessions#create', as: :user_session
+      delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
+    end
 
+    scope module: 'organizations' do
       namespace :admin do
       end
       
